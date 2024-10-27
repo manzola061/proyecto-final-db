0c85eab952f6aaf78c6ce666dc691e1a5371b5fb
Manuel Anzola <136187762+manzola016@users.noreply.github.com>
2024-10-27 11:13:55 -0400

    Add files via upload

---
+++ BaseProyectoFinal.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE BaseProyectoFinal;
+USE BaseProyectoFinal;
+
+CREATE TABLE Clientes(
+    Cedula_Cliente INT PRIMARY KEY,
+    Nombre_Cliente NVARCHAR(30),
+    Direccion_Cliente NVARCHAR(50),
+    Telefono_Cliente NVARCHAR(30)
+);
+
+CREATE TABLE Compras(
+    ID_Compra INT PRIMARY KEY IDENTITY(1, 1),
+    Cedula_Cliente INT,
+    Fecha_Compra DATE,
+    Monto_Total DECIMAL(10, 2),
+    FOREIGN KEY (Cedula_Cliente) REFERENCES Clientes(Cedula_Cliente)
+);
+
+SELECT * FROM Clientes;
+SELECT * FROM Compras;
